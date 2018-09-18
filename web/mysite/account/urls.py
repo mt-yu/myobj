668a53e1fa6b7d410a33a63f05c10b2f391c8f1f
mt-yu <30967271+mt-yu@users.noreply.github.com>
2018-09-18 16:38:34 +0800

    180918

--- web/mysite/account/urls.py
+++ web/mysite/account/urls.py
@@ -9,12 +9,12 @@ app_name='account'
 
 urlpatterns = [
     #views.user_login 意味着必须在views中创建一个名为user_login的函数响应请求
-
     #url(r'^login/$', views.user_login, name="user_login"),  #自定义的登录
+
     url(r'^login/$', auth_views.LoginView.as_view(), name="user_login"),  #django内置的登录
-    url(r'^new-login/$', auth_views.LoginView.as_view(), {"template_name": "account/login.html"}),
+    url(r'^new-login/$', auth_views.LoginView.as_view(), {"template_name": "account/login.html"}),  #修改template_name 属性的值，使用account/login.html
 
     #url(r'^logout/$', auth_views.LogoutView.as_view(), name="user_logout"), #内置django退出登录
-    path('logout/', auth_views.LogoutView.as_view(),{"template_name": "account/login.html"},name='user_logout')
-    #url(r'^logout/$', auth_views.LogoutView.as_view(), template_name="account/logout.html", name="user_logout"),
+    #path('logout/', auth_views.LogoutView.as_view(),{"template_name": "account/login.html"},name='user_logout')
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {"template_name" :"account/logout.html"}, name="user_logout"),
 ]
